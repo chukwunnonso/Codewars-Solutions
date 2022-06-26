e60ea03d9541e355d9f9cebe47268258e8e02154
chukwunnonso <cnnonso@gmail.com>
2022-06-26 03:54:01 -0700

    First solution

---
+++ js/solution001.js
@@ -0,0 +1,22 @@
+//Question: Return opposite number in Javascript
+
+ //P- Parameters
+ //R- Results
+ //E- Examples
+ //P- Pseudocodes
+
+
+
+
+//function opposite(number) {
+  //  return(-number);
+//}
+
+
+function oppositNumber(originalNumber){
+    return (-originalNumber);
+}
+console.log(oppositNumber(50))
+
+
+console.log('Beauty for ashes')
