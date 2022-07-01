809d6bb6bd9969aeedc0069e30b0f3f73fae1d3f
chukwunnonso <cnnonso@gmail.com>
2022-06-30 21:48:09 -1000

    Removed first and last character

---
+++ js/solution005.js
@@ -0,0 +1,35 @@
+// Remove First and Last Character
+
+// It's pretty straightforward.
+// Your goal is to create a function that removes the first and last characters of a string. 
+//You're given one parameter, the original string. 
+//You don't have to worry with strings with less than two characters.
+
+
+//P: Will we get another data type thats not a string? This will not apply for strings with less than two characters
+// What if we have more than two parameters, what would be the result? just thinking out loud.
+
+// R: Call the function and pass in a parameter
+// removeChar("Development")
+
+
+// E: "Development" = evelopmen
+// "Dominion" = "ominio"
+// "Beauty" = eaut
+
+// P: Pseudocode  
+function removeFirstAndLastChar(whatToRemove){
+    // introduce a method
+  
+    // return the result
+  }
+removeFirstAndLastChar("Diligence")  
+
+  function removeChar(str){
+    //You got this!
+    return str.slice(1, -1)
+   
+   };
+   console.log(removeChar("Magnificent"))
+  
+  
