081a80ca331a5e327689e9d0019df9575be1ea22
chukwunnonso <cnnonso@gmail.com>
2022-07-07 21:07:45 -1000

    Solution 12. Letss Gooo!

---
+++ js/solution012.js
@@ -0,0 +1,20 @@
+// Double an integer
+//P: we are working with integers. The goal is to have it doubled. 
+// R: console.log the result
+// E:
+// 50 -----> 100
+// 100 -----> 200
+
+// p:
+
+// write a function
+function integerTo(i) {
+
+    // let i be the integer, double it then return
+     return (i * 2);
+  }
+  // print answer in the console
+  console.log(integerTo(50))
+  
+
+
