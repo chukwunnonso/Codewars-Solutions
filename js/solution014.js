8ca0fb47ae55975f8d5ffcd4d863a410d17240db
chukwunnonso <cnnonso@gmail.com>
2022-07-09 23:25:46 -1000

    changed string to uppercase

---
+++ js/solution014.js
@@ -0,0 +1,21 @@
+//MakeUpperCase
+
+// Write a function which converts the input string to uppercase.
+
+//P: The data type we are working with is a String. It should be in lowerCase.
+// We are tasked with changing the entry to uppercase.
+
+// R: console.log(makeStringUpperCase("this is the string we are converting to lowercase"))
+//E: "this is the string we are converting to lowercase" ---> THIS IS THE STRING WE ARE CONVERTING TO LOWERCASE
+
+// P:
+
+// Write a function with a parameter of string.
+function makeStringUpperCase(str) {
+    // enter your code here
+    // Return the parameter with a toUpperCase method attached to it
+    return str.toUpperCase()
+  }
+  // print the result to the console.
+  console.log(makeStringUpperCase("change all these strings to uppercase"))
+  
