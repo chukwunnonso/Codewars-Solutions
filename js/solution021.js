c5f6680c03c433f490ee4502a4e8e86e67d3f93d
chukwunnonso <cnnonso@gmail.com>
2022-07-16 23:41:48 -1200

    simple arrow function

---
+++ js/solution021.js
@@ -0,0 +1,16 @@
+// Function 3 - multiplying two numbers
+
+// Implement a function which mulltiplies two numbers 
+
+//P: working with two positive integers. We are to mulitply them both. Function name is "multiply"
+//R: console.log(multiply(30, 5), 150)
+// console.log(multiply(80, 20), 1600)
+
+//E: 80, 20 = 1600
+//   60, 30 = 1800
+
+// P: write an arrow function with the name multiply. Because we have two parameters, a parenthesis is needed.
+// After the parenthesis we have our expression which is a multiplication.
+// call the function
+const multiply = (num1, num2) => num1 * num2
+console.log(multiply(50, 6))
