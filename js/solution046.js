a7c2534445664a8bfb6831e18638b75de5569a4a
chukwunnonso <cnnonso@gmail.com>
2022-08-09 16:48:36 -1200

    array average

---
+++ js/solution046.js
@@ -0,0 +1,27 @@
+/*
+ Get the mean of an arrayed
+
+
+
+It's the academic year's end, fateful moment of your school report. The averages must be calculated. 
+All the students come to you and entreat you to calculate their average for them. Easy !
+You just need to write a script.
+
+Return the average of the given array rounded down to its nearest integer.
+
+The array will never be empty.
+
+*/
+
+
+
+
+
+
+
+
+function getAverage(marks) {
+    //TODO : calculate the downwar rounded average of the marks array
+    return Math.floor(marks.reduce((acc, c) => acc + c) / marks.length);
+}
+console.log(getAverage([20, 30, 50, 40, 60, 70, 90]))
