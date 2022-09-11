c82c294c6856d5fbc40b66c4cae140df1f865443
chukwunnonso <cnnonso@gmail.com>
2022-09-10 23:54:46 -0700

    for loop

---
+++ js/solution071.js
@@ -0,0 +1,28 @@
+/*
+Counting the sheeps
+
+ If you can't sleep, just count sheep!!
+
+Task:
+Given a non-negative integer, 3 for example, return a string with a murmur:
+"1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+var countSheep = function (num){
+  //your code here
+}
+*/
+
+
+var countSheep = function (num) {
+    let str = "";
+    for (i = 1; i <= num; i++) {
+      str += `${i} sheep...`
+    }
+    return str
+}
+console.log(countSheep(5))
+ 
+
+
+
+
